a44105edc1cfd03c6caf87c46355540217f333c1
Drew Killeen <drewkilleen00@gmail.com>
2022-05-27 14:33:54 -0400

    implement ConfigFields class

--- src/index.js
+++ src/index.js
@@ -1,44 +1,89 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 const axios = require("axios");
 
-let ip = "localhost";
-let port = "1025";
-const timersUrl = "http://" + ip + ":" + port + "/v1" + "/timers?chunked=false";
+let ip = "";
+let port = "";
 
-const fetchTimerData = () => {
-  console.log("running");
-  axios.get(timersUrl).then((response) => {
-    console.log(!response.data);
-    const timers = response.data;
-    this.setState({ timers });
-  });
+const fetchData = () => {
+  return axios
+    .get("http://" + ip + ":" + port + "/v1" + "/timers?chunked=false")
+    .then((response) => response.data);
 };
 
-class Config extends Component {
+class ConfigFields extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ip: "localhost",
+      port: "1025",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    ip = this.state.ip;
+    port = this.state.port;
+  }
+
   render() {
-    return <div>Config</div>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          IP Address:
+          <input
+            name="ip"
+            type="text"
+            value={this.state.ip}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Port:
+          <input
+            name="port"
+            type="text"
+            value={this.state.port}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
 
-class Countdown extends Component {
+class Countdown extends React.Component {
   render() {
     return <div>Countdown</div>;
   }
 }
-class Elapsed extends Component {
+class Elapsed extends React.Component {
   render() {
     return <div>Elapsed</div>;
   }
 }
-class CountDownToTime extends Component {
+class CountDownToTime extends React.Component {
   render() {
     return <div>CountDownToTime</div>;
   }
 }
 
-class TimerContainer extends Component {
+class TimerContainer extends React.Component {
   state = {
     timers: [],
   };
@@ -66,4 +111,4 @@ class TimerContainer extends Component {
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Config />);
+root.render(<ConfigFields />);
