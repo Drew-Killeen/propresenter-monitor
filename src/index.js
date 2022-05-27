1f5c6e792081633b2e4248bd74544204f3176683
Drew Killeen <drewkilleen00@gmail.com>
2022-05-27 11:35:57 -0400

    add config class

--- src/index.js
+++ src/index.js
@@ -1,43 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 const axios = require("axios");
 
-const url =
-  "http://" + "localhost" + ":" + "1025" + "/v1" + "/timers?chunked=false";
+let ip = "localhost";
+let port = "1025";
+const timersUrl = "http://" + ip + ":" + port + "/v1" + "/timers?chunked=false";
 
-class Timer extends React.Component {
+const fetchTimerData = () => {
+  console.log("running");
+  axios.get(timersUrl).then((response) => {
+    console.log(!response.data);
+    const timers = response.data;
+    this.setState({ timers });
+  });
+};
+
+class Config extends Component {
+  render() {
+    return <div>Config</div>;
+  }
+}
+
+class TimerContainer extends Component {
   state = {
     timers: [],
   };
-
-  componentDidMount() {
-    console.log("running");
-    axios.get(url).then((response) => {
-      console.log(!response.data);
-      const timers = response.data;
-      this.setState({ timers });
-    });
-  }
 
   render() {
     return (
       <ul>
         {this.state.timers.map((timer) => {
-          let timerType;
-          if (timer.countdown) {
-            console.log("countdown");
-            timerType = timer.countdown.duration;
-          } else if (timer.count_down_to_time) {
-            console.log("countdown to time");
-            timerType = timer.count_down_to_time.time_of_day;
-          } else {
-            console.log("elapsed");
-            timerType = timer.elapsed.start_time;
-          }
-          <li key={timer.id.index}>
-            {timer.id.name} - {timerType}
-          </li>;
+          // let timerType;
+          // if (timer.countdown) {
+          //   console.log("countdown");
+          //   timerType = timer.countdown.duration;
+          // } else if (timer.count_down_to_time) {
+          //   console.log("countdown to time");
+          //   timerType = timer.count_down_to_time.time_of_day;
+          // } else {
+          //   console.log("elapsed");
+          //   timerType = timer.elapsed.start_time;
+          // }
+          <li key={timer.id.index}>{timer.id.name}</li>;
         })}
       </ul>
     );
@@ -45,4 +50,4 @@ class Timer extends React.Component {
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Timer />);
+root.render(<Config />);
